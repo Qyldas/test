7f5f856420889a1e169f1ef84c29ab2761c28348
Qyldas <lucash71530@gmail.com>
2022-07-11 19:19:31 +0200

    second commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+  {
+    path: "", loadChildren:()=> import("./home-page/home-page.module").then(m=> m.HomePageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
